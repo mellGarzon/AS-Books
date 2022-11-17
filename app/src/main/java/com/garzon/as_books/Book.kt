9bad1b0be599f695c9819a0f4f529bbbc47f94c3
LucasBrentano <brentano.l@gmail.com>
2022-11-17 19:33:31 -0300

    book wip

--- app/src/main/java/com/garzon/as_books/Book.kt
+++ app/src/main/java/com/garzon/as_books/Book.kt
@@ -1,4 +1,11 @@
 package com.garzon.as_books
 
-data class Book(var pageNumber: Int, var bookMark: Int, var type: String, var author: String, var title: String, var isRead: Boolean)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Book(var id: Int?, var pageNumber: Int, var bookMark: Int, var type: String, var author: String, var title: String, var isRead: Boolean): Parcelable {
+   constructor(parcel: Parcel) : this(
+       parcel.readValue(Int::class.java.classLoader) as? Int
+   )
+}
 
